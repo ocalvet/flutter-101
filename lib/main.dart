aaf2390fe78e100df17d8733d96a3250e38e374e
Ovidio Calvet <calveto@gmail.com>
2018-07-19 06:57:02 -0400

    Add sample to do request from site

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_101/Post.dart';
 import 'HelloText.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  if (response.statusCode == 200) {
+    // If server returns an OK response, parse the JSON
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // If that response was not OK, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -9,11 +27,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Tester',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Testing flutter'),
-        ),
-        body: HelloText(text: 'Welcome!!!')
-      ),
-    );
+          appBar: AppBar(
+            title: Text('Testing flutter'),
+          ),
+          body: Center(
+            child: FutureBuilder<Post>(
+              future: fetchPost(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.title);
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+
+                // By default, show a loading spinner
+                return CircularProgressIndicator();
+              },
+            ),
+          )
+        )
+      );
   }
 }
