07483e1b4ff60b54b9da6ea31c3dac964ce98175
Ovidio R. Calvet <calveto@gmail.com>
2018-07-22 23:27:50 -0400

    Add Todo Item

---
+++ lib/TodoItem.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TodoItem extends StatelessWidget {
+  TodoItem();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Todo Item')
+    );
+  }
+}
--- lib/TodoList.dart
+++ lib/TodoList.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_101/TodoItem.dart';
 
 class TodoList extends StatelessWidget {
   TodoList();
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Todo List')
+      child: Column(
+        children: <Widget>[
+          Text('Todo List'),
+          TodoItem(),
+        ]
+      )
     );
   }
 }
