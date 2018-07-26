8f28258273715344ff6d4a20d8459ae9eea5d411
Ovidio R. Calvet <calveto@gmail.com>
2018-07-25 22:06:56 -0400

    Add text field

--- lib/TodoForm.dart
+++ lib/TodoForm.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 class TodoForm extends StatelessWidget {
   TodoForm({this.onAddTodo});
   final Function onAddTodo;
+  changeText(text) {
+    print(text);
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Todo Form')
+      child: Row(children: <Widget>[
+        TextField(onChanged: this.changeText),
+
+      ],)
     );
   }
 }
