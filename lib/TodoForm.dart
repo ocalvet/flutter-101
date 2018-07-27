1c560aa203019e215b500de4408a6e45dfbff3b4
Ovidio R. Calvet <calveto@gmail.com>
2018-07-26 22:07:36 -0400

    Add way to clear text

--- lib/TodoForm.dart
+++ lib/TodoForm.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
 
-class TodoForm extends StatelessWidget {
-  var todoTitle;
+class TodoForm extends StatelessWidget{
   TodoForm({this.onAddTodo});
   final Function onAddTodo;
+  final TextEditingController _controller = new TextEditingController();
+  String todoTitle;
   changeText(text) {
-    print("First text field: $text");
     this.todoTitle = text;
   }
   @override
   Widget build(BuildContext context) {
+    TextField textField = TextField(onChanged: this.changeText, controller: _controller);
     return Container(
       child: Column(children: <Widget>[
-        TextField(onChanged: this.changeText),
+        textField,
         RaisedButton(
           child: Text("Add Todo"),
           onPressed: () {
-            this.onAddTodo(this.todoTitle);
+            if (this.todoTitle != null && this.todoTitle.length > 0) {
+              this.onAddTodo(this.todoTitle);
+              _controller.clear();
+              this.todoTitle = "";  
+            }
           })
       ],)
     );
