7aa9283ceede2d717242e30d1623a80e75f010d6
Ovidio R. Calvet <calveto@gmail.com>
2018-07-30 19:33:30 -0400

    Remove unused code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,14 @@
 // are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_101/TodoApp.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_101/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new TodoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
